bf5b494efd51a1e2fb276b46ea08421ca7502c21
Filippo Guida <filippo.guida@icloud.com>
2019-11-13 15:06:26 +0100

    move from WebMidi to JZZ library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { midiConnect, midiOut, midiIn } from "./modules/midi";
+import JZZ from "jzz";
 import { Dial, Sequencer, Toggle } from "react-nexusui";
 
 function App() {
+  var midiIn = JZZ().openMidiIn();
+  var midiOut = JZZ().openMidiOut();
+
   //CC Looper
+  /*
   let looperTime = 0;
   let clearLooper = false;
   midiConnect().then(() => {
@@ -15,58 +19,92 @@ function App() {
         if (!clearLooper) looper(e);
       }, looperTime);
     }
+  });*/
+
+  //
+  let empty = JZZ.Widget({
+    _receive: function(msg) {
+      console.log(msg);
+      this.emit(msg);
+    }
   });
+  empty.connect(midiOut);
 
   //Sequencer
   let sequencer = null;
-  let playing = false;
+  let timeline;
   function next() {
-    if (playing) sequencer.next();
-    setTimeout(next, 100);
+    sequencer.next();
+    timeline = setTimeout(next, 100);
   }
-  next();
+  let play = () => next();
+  let stop = () => clearTimeout(timeline);
+
+  let probSteps = Array(16).fill(1);
+  let probTracks = Array(4).fill(1);
+  function playStep(triggers) {
+    for (let i = 0; i < triggers.length; i++)
+      if (
+        triggers[i] &&
+        probSteps[i] > Math.random() &&
+        probTracks[i] > Math.random()
+      ) {
+        console.log(probSteps[i]);
+        empty
+          .noteOn(0, "C#5", 127)
+          .wait(500)
+          .noteOff(0, "C#4");
+      }
+  }
 
   return (
     <div className="App">
       <main>
         <div className="dials-step-container">
-          {[...Array(16).keys()].map(val => (
-            <div key={"dial-step-div" + val} className="dial-step">
+          {[...Array(16).keys()].map(i => (
+            <div key={"dial-step-div" + i} className="dial-step">
               <Dial
-                key={"dial-step-" + val}
+                key={"dial-step-" + i}
                 min={0}
-                max={10}
+                max={1}
+                value={probSteps[i]}
+                interaction="vertical"
                 size={[100, 100]}
-                onChange={val => (looperTime = val)}
+                onChange={val => (probSteps[i] = val)}
               />
             </div>
           ))}
         </div>
         <div className="sequencer-container">
           <Sequencer
-            min={0}
-            max={10}
             columns={16}
             rows={4}
             size={[1600, 400]}
             onReady={seq => (sequencer = seq)}
-            onStep={playStep}
+            onStep={triggers => playStep(triggers)}
           />
         </div>
         <div className="dials-track-container">
-          {[...Array(4).keys()].map(val => (
-            <div key={"dial-track-div" + val} className="dial-track">
+          {[...Array(4).keys()].map(i => (
+            <div key={"dial-track-div" + i} className="dial-track">
               <Dial
-                key={"dial-track-" + val}
+                key={"dial-track-" + i}
                 min={0}
-                max={10}
+                max={1}
+                value={probTracks[i]}
                 size={[100, 100]}
-                onChange={val => (looperTime = val)}
+                onChange={val => (probTracks[i] = val)}
               />
             </div>
           ))}
         </div>
-        <Toggle state={false} onChange={state => (playing = state)} />
+        <Toggle
+          state={false}
+          onChange={playing => {
+            if (playing) play();
+            else stop();
+          }}
+        />
       </main>
     </div>
   );
